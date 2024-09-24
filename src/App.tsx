4523dc7cd501c5f460b390d31e5c97c84b79d56a
Andrew Anil George <andrewpularickal@gmail.com>
2024-09-24 00:58:52 -0400

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import WorkExperience from './components/WorkExperience';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Resume from './components/Resume';
-import Footer from './components/Footer'
 
 const App: React.FC = () => {
   return (
@@ -15,12 +14,11 @@ const App: React.FC = () => {
       <div className="pt-16">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/resume" element={<Resume />} />
           <Route path="/work-experience" element={<WorkExperience />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="/resume" element={<Resume />} />
         </Routes>
-        <Footer />
       </div>
     </Router>
   );
