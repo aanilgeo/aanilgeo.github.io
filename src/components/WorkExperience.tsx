a683ba37788d0d790d4b33f03ca86ead54a0d2e4
Andrew Anil George <andrewpularickal@gmail.com>
2024-12-11 08:29:08 -0500

    Updated Work Experience

--- src/components/WorkExperience.tsx
+++ src/components/WorkExperience.tsx
@@ -9,7 +9,7 @@ const WorkExperience: React.FC = () => {
     >
       <h1 className="text-4xl font-bold mb-8 text-gray-800 dark:text-white">Work Experience</h1>
       <div className="grid gap-6 max-w-2xl w-full mb-10">
-        {/* Experience 1: Software Engineering Intern at Jacobs */}
+        {/* Experience 1: Software Engineer Co-op at Honeywell */}
         <div className="bg-white dark:bg-gray-900 shadow-md p-4 rounded-lg flex items-center space-x-4 justify-start transition-transform duration-300 hover:scale-105 hover:shadow-lg">
           <img
             src={getCompanyLogo('honeywell.com')}
@@ -18,7 +18,7 @@ const WorkExperience: React.FC = () => {
           />
           <div className="text-left">
             <h2 className="text-xl font-semibold text-gray-800 dark:text-white">
-              Incoming Software Engineer Co-op
+              Software Engineer Co-op
             </h2>
             <h3 className="text-gray-700 dark:text-gray-300">Honeywell</h3>
             <h4 className="text-gray-500 dark:text-gray-400">Mason, OH</h4>
